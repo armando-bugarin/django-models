d825a9a1810fa19e0b4953f2048a3779d3bd4f3d
Armando Bugarin <armando.bugarin24@gmail.com>
2024-02-14 22:45:34 -0800

    edit README; add tests.py

--- README.md
+++ README.md
@@ -8,11 +8,10 @@
 
 - `pip install pytest-watch`
 - `pip install django`
-- `npm install -D tailwindcss`
 
 ### Tests
 
 - How do you run tests? `python manage.py test`
 - How to run server? `python manage.py runserver`
-- How to run watch? `npx tailwindcss -i ./static/src/input.css -o ./static/src/output.css --watch`
-- Any tests of note?
+- How to create and apply migrations: `python manage.py makemigration`, and `python manage.py migrate`.
+- Any tests of note? **There are two tests.**
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+class ThingTests(TestCase):
+    def test_list_page_status_code(self):
+        url = reverse("thing_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse("thing_list")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "thing_list.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    ## tests for Detail Page are stretch
+
 
 # Create your tests here.
